36d60db9e31eed5c96c7500a5616014867d32a23
nikkinicholasromero <nikkinicholas.romero@gmail.com>
2024-10-06 06:36:40 +0800

    Added sample logic

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import http from "http";
+import { sum } from "./core";
 
 const requestHandler = (
   req: http.IncomingMessage,
   res: http.ServerResponse,
 ): void => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World");
+  res.end("Hello, World: " + sum(2, 5));
 };
 
 const server = http.createServer(requestHandler);
