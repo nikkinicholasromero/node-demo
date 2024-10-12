515558cc715cb2725b3441361b11e172d7fa1da3
nikkinicholasromero <nikkinicholas.romero@gmail.com>
2024-10-13 04:40:59 +0800

    Added 404

--- src/router.ts
+++ src/router.ts
@@ -4,21 +4,27 @@ import WebSocket from "ws";
 
 const initializeRouter = (wss: WebSocket.Server) => {
   return (req: http.IncomingMessage, res: http.ServerResponse): void => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(
-      "Hello, World: " +
-        sum(2, 5) +
-        ": " +
-        process.env.LOGNAME +
-        ": " +
-        process.env.ABC,
-    );
-
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send("HTTP route was accessed!");
       }
     });
+
+    if (req.url === "" || req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(
+        "Hello, World: " +
+          sum(2, 5) +
+          ": " +
+          process.env.LOGNAME +
+          ": " +
+          process.env.ABC,
+      );
+      return;
+    }
+
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
   };
 };
 
